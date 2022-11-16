9231ed85b60a60432b9b4c7650fb8f49513aff36
Ilya <topor.ilyaa@gmail.com>
2022-11-16 17:01:32 +0100

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Илья on 16/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
